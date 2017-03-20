d557737164ad1895106116573cddfa111c4b6de2
NickNevolko <nikolay.nevolko@gmail.com>
2017-03-20 23:48:59 +0300

    newton task added

---
+++ NUnit.TestsNewton/TestNewtonMethod.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using static NewtonSqrt.NewtonTask;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.TestsNewton
+{
+    [TestFixture]
+    public class TestNewtonMethod
+    { 
+
+        [TestCase(100, 2, 0.0000000001d, ExpectedResult = 10)]
+        [TestCase(9, 2, 0.000000000001d, ExpectedResult = 3)]
+        [TestCase(64, 2, 0.00000000001d, ExpectedResult = 8)]
+        [TestCase(81, 4, 0.000000000001d, ExpectedResult = 3)]
+        [TestCase(4, 2, 0.000000000001d, ExpectedResult = 2)]
+        public double TestNewton(double number, int n, double eps)
+        {
+            return SqrtN(number, n, eps);
+        }
+
+        [TestCase(-5, 2, 0.01d)]
+        [TestCase(5, 2, -0.01d)]
+        public void TestNewtonIncorrect(double number, int n, double eps)
+        {
+            Assert.Throws<ArgumentException>(() => SqrtN(number, n, eps));
+        }
+    }
+}
---
+++ NewtonSqrt/NewtonTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonSqrt
+{
+    /// <summary>
+    /// static class with one method
+    /// that calculate  n-root
+    /// </summary>
+    public static class NewtonTask
+    {
+        /// <summary>
+        /// calculate root using Newton Method
+        /// </summary>
+        /// <param name="number">number of root to be founded</param>
+        /// <param name="n">power of the number</param>
+        /// <param name="eps">allowable error</param>
+        /// <returns>returns a n-root of number</returns>
+        public static double SqrtN(double number, int n, double eps)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException(nameof(eps));
+            }
+
+            if (eps < 0)
+            {
+                throw new ArgumentException(nameof(eps));
+            }
+
+            double x = number / 2;
+            double xn = number;
+
+            while (Math.Abs(x - xn) > eps)
+            {
+                xn = x;
+                x = ((n - 1)*x + number/ Math.Pow(x, n - 1)) / n;
+            }
+
+            return x;
+        }
+    }
+}
